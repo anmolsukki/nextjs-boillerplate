304e60e68f46d13f8c7a9047256654b63d2277ad
anmolsukki <anmolsukki94@gmail.com>
2021-01-17 07:19:51 +0530

    404 page

---
+++ pages/404.js
@@ -0,0 +1,30 @@
+export default function Custom404() {
+  return (
+    <div className="notFound">
+      <section className="error">
+        <div className="content">
+          <canvas className="snow" id="snow" />
+          <div className="main-text">
+            <h1>
+              <div>We couldn’t find the page you’re looking for.</div>
+              <div className="subtext">
+                <div className="subtextsub">You may have clicked the wrong link or mistyped the address. Please use the links below.</div>
+              </div>
+            </h1>
+            <div>
+              <a className="button" href="/">
+                <span>Go Back to Home</span>
+              </a>
+            </div>
+            <div className="ground">
+              <div className="mound">
+                <div className="mound_text">404</div>
+                <div className="mound_spade" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
