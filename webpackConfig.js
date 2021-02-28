f725220a1550e11cb717967431b5835a5ec8a587
anmolsukki <anmolsukki94@gmail.com>
2021-02-28 21:16:25 +0530

    Fixes

--- webpackConfig.js
+++ webpackConfig.js
@@ -1,39 +1,35 @@
-module.exports = (config, { dev, isServer }) => {
-  const _config = config;
-  if (dev) {
-    _config.module.rules.push(
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        exclude: /node_modules/,
-        loader: 'file-loader'
-      }
-    );
-  }
+const path = require('path');
 
-  if (isServer) {
-    const antStyles = /antd\/.*?\/style.*?/;
-    const origExternals = [..._config.externals];
-    _config.externals = [
-      (context, request, callback) => {
-        if (request.match(antStyles)) return callback();
-        if (typeof origExternals[0] === 'function') {
-          origExternals[0](context, request, callback);
-        } else {
-          return callback();
+module.exports = (config) => {
+  const webConfig = config;
+  webConfig.module.rules.push(
+    {
+      test: require.resolve('jquery'),
+      use: [
+        {
+          loader: 'expose-loader',
+          options: 'jQuery'
+        },
+        {
+          loader: 'expose-loader',
+          options: '$'
+        },
+        {
+          loader: 'expose-loader',
+          options: 'jquery'
         }
-        return null;
-      },
-      ...(typeof origExternals[0] === 'function' ? [] : origExternals)
-    ];
-    _config.module.rules.unshift({
-      test: antStyles,
-      use: 'null-loader'
-    });
-  }
-  return _config;
+      ]
+    },
+    {
+      test: /\.js|\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader'
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      exclude: /node_modules/,
+      loader: 'file-loader'
+    }
+  );
+  return webConfig;
 };
